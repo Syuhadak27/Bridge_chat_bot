0fa65671120d90be50a5792eb1537f0b846ea66a
Syuhadak27 <117827583+Syuhadak27@users.noreply.github.com>
2025-02-08 16:07:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const BOT_TOKEN = "8199922853:AAFKj9W4Sqow7LIBJ2CaSxrvs6te32_rrTs";
-export const ADMIN_ID = "1980888203"; // Ganti dengan ID admin atau grup
-export const webhookUrl = "https://bot-forward.henot20561.workers.dev";
+export const BOT_TOKEN = "";
+export const ADMIN_ID = ""; // Ganti dengan ID admin atau grup
+export const webhookUrl = "";
